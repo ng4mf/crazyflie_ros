8904cb7e7dfb4ac514b0b9c07fefec6fde683a30
ng4mf <ng4mf@users.noreply.github.com>
2021-02-22 13:24:54 -0500

    commit before switching back to Dec. 10 for more sim experiments

--- crazyflie_demo/scripts/publish_pose.py
+++ crazyflie_demo/scripts/publish_pose.py
@@ -28,58 +28,10 @@ if __name__ == '__main__':
     msg.pose.orientation.z = quaternion[2]
     msg.pose.orientation.w = quaternion[3]
 
-    msg2 = PoseStamped()
-    msg2.header.seq = 0
-    msg2.header.stamp = rospy.Time.now()
-    msg2.header.frame_id = worldFrame
-    msg2.pose.position.x = x - 2
-    msg2.pose.position.y = y - 0.5
-    msg2.pose.position.z = z - 0.5
-    quaternion = tf.transformations.quaternion_from_euler(0, 0, 0)
-    msg2.pose.orientation.x = quaternion[0]
-    msg2.pose.orientation.y = quaternion[1]
-    msg2.pose.orientation.z = quaternion[2]
-    msg2.pose.orientation.w = quaternion[3]
-
-    msg3 = PoseStamped()
-    msg3.header.seq = 0
-    msg3.header.stamp = rospy.Time.now()
-    msg3.header.frame_id = worldFrame
-    msg3.pose.position.x = x
-    msg3.pose.position.y = y - 1.5
-    msg3.pose.position.z = z - 1
-    quaternion = tf.transformations.quaternion_from_euler(0, 0, 0)
-    msg3.pose.orientation.x = quaternion[0]
-    msg3.pose.orientation.y = quaternion[1]
-    msg3.pose.orientation.z = quaternion[2]
-    msg3.pose.orientation.w = quaternion[3]
-
     pub = rospy.Publisher(name, PoseStamped, queue_size=1)
-
-    modder = 100
 
     while not rospy.is_shutdown():
         msg.header.seq += 1
         msg.header.stamp = rospy.Time.now()
         pub.publish(msg)
-        # if msg.header.seq < 900:
-        #     if msg.header.seq % modder == 0:
-        #         rospy.loginfo("1st WAYPOINT")
-        #     msg.header.seq += 1
-        #     msg.header.stamp = rospy.Time.now()
-        #     pub.publish(msg)
-        # elif msg.header.seq < 1500:
-        #     if msg2.header.seq % modder == 0:
-        #         rospy.loginfo("2nd WAYPOINT")
-        #     msg.header.seq += 1
-        #     msg2.header.seq = msg.header.seq
-        #     msg2.header.stamp = rospy.Time.now()
-        #     pub.publish(msg2)
-        # else:
-        #     if msg3.header.seq % modder == 0:
-        #         rospy.loginfo("3rd WAYPOINT")
-        #     msg.header.seq += 1
-        #     msg3.header.seq = msg.header.seq
-        #     msg3.header.stamp = rospy.Time.now()
-        #     pub.publish(msg3)
         rate.sleep()
